3a1053e4205d41abcc8afa3026e34846d40b22d4
Barath s <112713529+freqcoder19@users.noreply.github.com>
2025-04-10 18:22:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-
 const corsOptions = {
     origin:'http://localhost:5173',
     credentials:true
@@ -21,14 +20,21 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+
 // api's
 app.use("/api/v1/user", userRoute);
 
-"https://localhost:8000/api/v1/user/register"
-"https://localhost:8000/api/v1/user/login"
-"https://localhost:8000/api/v1/user/profile/update"
+"https://localhost:3000/api/v1/user/register"
+"https://localhost:3000/api/v1/user/login"
+"https://localhost:3000/api/v1/user/profile/update"
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT,()=>{
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+  });
+
+  app.get('/', (req, res) => {
+    res.send(' Server is working fine!');
+  });
+
+
+  
